d834f6b51ee5b35a9055ed06d02c6eab127f7d36
RiaVenice <riatcruz.v1@gmail.com>
2025-11-21 15:07:05 +0800

    editing css

--- app/Views/view_equipments.php
+++ app/Views/view_equipments.php
@@ -1,7 +1,9 @@
+
 <div class="wrapper">
     <?= view('include/view_head') ?>
     <?= view('include/view_sidebar') ?>
     <div class="main-content">
         <!-- page content -->
+         
     </div>
 </div>
