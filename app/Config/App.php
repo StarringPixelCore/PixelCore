835f59fbbc2e9c17ed874e242c9c67bff29c9af3
RiaVenice <riatcruz.v1@gmail.com>
2025-11-21 14:02:18 +0800

    fully fixed routing

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/PixelCore/public/';
+    public string $baseURL = 'http://localhost/PixelCore/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
--- app/Views/view_index.php
+++ app/Views/view_index.php
@@ -122,34 +122,7 @@
 
 <body>
 
-    <!-- SIDEBAR -->
-    <div class="sidebar">
-        <div class="text-center mb-4">
-            <img src="<?= base_url('assets/feutech.png') ?>" width="120">
-        </div>
-
-        <a href="<?= base_url('dashboard') ?>"
-        class="menu-item <?= ($active=='dashboard') ? 'active' : '' ?>">DASHBOARD</a>
-
-        <a href="<?= base_url('equipments') ?>"
-        class="menu-item <?= ($active=='equipments') ? 'active' : '' ?>">EQUIPMENTS</a>
-
-        <a href="<?= base_url('reservations') ?>"
-        class="menu-item <?= ($active=='reservations') ? 'active' : '' ?>">RESERVATIONS</a>
-
-        <a href="<?= base_url('borrowed') ?>"
-        class="menu-item <?= ($active=='borrowed') ? 'active' : '' ?>">BORROWED</a>
-
-        <a href="<?= base_url('returned') ?>"
-        class="menu-item <?= ($active=='returned') ? 'active' : '' ?>">RETURNED</a>
-
-
-        <!-- PROFILE (placeholder na muna waiting for backend) -->
-        <div class="profile-box mt-5">
-            <img src="">
-            <p class="m-0" style="color:#F7E967"></p>
-        </div>
-    </div>
+   <?= view('include/view_sidebar') ?>
 
     <!-- MAIN CONTENT -->
     <div class="dashboard-container">
