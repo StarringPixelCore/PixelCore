939e4b9fa3ff151be3f8296c9ffb3cb30a4f0f1b
a-manalo <agmnlo.by@gmail.com>
2025-11-23 03:51:18 +0800

    added form validation for add user, edit user, and login

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -7,6 +7,7 @@ use CodeIgniter\Validation\StrictRules\CreditCardRules;
 use CodeIgniter\Validation\StrictRules\FileRules;
 use CodeIgniter\Validation\StrictRules\FormatRules;
 use CodeIgniter\Validation\StrictRules\Rules;
+use App\Validation\UserRules;
 
 class Validation extends BaseConfig
 {
@@ -25,6 +26,7 @@ class Validation extends BaseConfig
         FormatRules::class,
         FileRules::class,
         CreditCardRules::class,
+        UserRules::class,
     ];
 
     /**
@@ -41,4 +43,167 @@ class Validation extends BaseConfig
     // --------------------------------------------------------------------
     // Rules
     // --------------------------------------------------------------------
+
+    public array $login = [
+        'id_number' => [
+            'label' => 'ID Number',
+            'rules' => 'required|id_number_exists',
+            'errors' => [
+                'required' => 'Please enter your ID number.',
+                'id_number_exists' => 'The ID number you entered is not connected to an account.'
+            ]
+        ],
+
+        'password' => [
+            'label' => 'Password',
+            'rules' => 'required|valid_password[id_number]',
+            'errors' => [
+                'required' => 'Please enter your password.',
+                'valid_password' => 'Your password is incorrect.'
+            ]
+        ]
+    ];
+
+    public array $signup = [
+        'id_number' => [
+            'label' => 'ID Number',
+            'rules' => 'required|min_length[3]|max_length[50]|is_unique[tblusers.id_number]',
+            'errors' => [
+                'required' => 'Please enter an ID number.',
+                'min_length' => 'ID number must be at least 8 characters long.',
+                'max_length' => 'ID number must not exceed 50 characters.',
+                'is_unique' => 'This ID number is already registered. Please use a different one.'
+            ]
+        ],
+        
+        'firstname' => [
+            'label' => 'First Name',
+            'rules' => 'required|min_length[2]|max_length[100]|regex_match[/^[A-Z][a-zA-Z.\'\- ]*$/]',
+            'errors' => [
+                'required' => 'Please enter your first name.',
+                'min_length' => 'Your first name must be at least 2 characters long.',
+                'max_length' => 'Your first name must not exceed 100 characters.',
+                'regex_match' => 'First name must start with a capital letter and can only contain letters, spaces, periods, dashes, and apostrophes.'
+            ]
+        ],
+
+        'lastname' => [
+            'label' => 'Last Name',
+            'rules' => 'required|min_length[2]|max_length[100]|regex_match[/^[A-Z][a-zA-Z.\'\- ]*$/]',
+            'errors' => [
+                'required' => 'Please enter your last name.',
+                'min_length' => 'Your last name must be at least 2 characters long.',
+                'max_length' => 'Your last name must not exceed 100 characters.',
+                'regex_match' => 'Last name must start with a capital letter and can only contain letters, spaces, periods, dashes, and apostrophes.'
+            ]
+        ],
+
+        'email' => [
+            'label' => 'Email',
+            'rules' => 'required|valid_email|is_unique[tblusers.email]',
+            'errors' => [
+                'required' => 'Please enter your email address.',
+                'valid_email' => 'Please enter a valid email address.',
+                'is_unique' => 'This email address is already registered. Please use a different one.'
+            ]
+        ],
+
+        'password' => [
+            'label' => 'Password',
+            'rules' => 'required|min_length[8]|max_length[50]',
+            'errors' => [
+                'required'   => 'Please enter a password.',
+                'min_length' => 'Your password must be at least 8 characters long.',
+                'max_length' => 'Your password must not exceed 50 characters.'
+            ]
+        ],
+
+        'confirmpassword' => [
+            'label' => 'Confirm Password',
+            'rules' => 'required|matches[password]',
+            'errors' => [
+                'required' => 'Please confirm your password.',
+                'matches' => 'Your passwords do not match. Please try again.'
+            ]
+        ],
+
+        'role' => [
+            'label' => 'Role',
+            'rules' => 'required|in_list[USER,ITSO PERSONNEL,STUDENT,ASSOCIATE]',
+            'errors' => [
+                'required' => 'Please select a role.',
+                'in_list' => 'Please select a valid role.'
+            ]
+        ]
+    ];
+
+    public array $editAccount = [
+        'id_number' => [
+            'label' => 'ID Number',
+            'rules' => 'required|min_length[3]|max_length[50]',
+            'errors' => [
+                'required' => 'Please enter your ID number.',
+                'min_length' => 'Your ID number must be at least 8 characters long.',
+                'max_length' => 'Your ID number must not exceed 50 characters.'
+            ]
+        ],
+        
+        'firstname' => [
+            'label' => 'First Name',
+            'rules' => 'required|min_length[2]|max_length[100]|regex_match[/^[A-Z][a-zA-Z.\'\- ]*$/]',
+            'errors' => [
+                'required' => 'Please enter your first name.',
+                'min_length' => 'Your first name must be at least 2 characters long.',
+                'max_length' => 'Your first name must not exceed 100 characters.',
+                'regex_match' => 'First name must start with a capital letter and can only contain letters, spaces, periods, dashes, and apostrophes.'
+            ]
+        ],
+
+        'lastname' => [
+            'label' => 'Last Name',
+            'rules' => 'required|min_length[2]|max_length[100]|regex_match[/^[A-Z][a-zA-Z.\'\- ]*$/]',
+            'errors' => [
+                'required' => 'Please enter your last name.',
+                'min_length' => 'Your last name must be at least 2 characters long.',
+                'max_length' => 'Your last name must not exceed 100 characters.',
+                'regex_match' => 'Last name must start with a capital letter and can only contain letters, spaces, periods, dashes, and apostrophes.'
+            ]
+        ],
+
+        'email' => [
+            'label' => 'Email',
+            'rules' => 'required|valid_email',
+            'errors' => [
+                'required' => 'Please enter your email address.',
+                'valid_email' => 'Please enter a valid email address.'
+            ]
+        ],
+
+        'password' => [
+            'label' => 'Password',
+            'rules' => 'permit_empty|min_length[8]|max_length[50]',
+            'errors' => [
+                'min_length' => 'Your password must be at least 8 characters long.',
+                'max_length' => 'Your password must not exceed 50 characters.'
+            ]
+        ],
+
+        'confirmpassword' => [
+            'label' => 'Confirm Password',
+            'rules' => 'permit_empty|matches[password]',
+            'errors' => [
+                'matches' => 'Your passwords do not match. Please check again.'
+            ]
+        ],
+
+        'role' => [
+            'label' => 'Role',
+            'rules' => 'required|in_list[USER,ITSO PERSONNEL,STUDENT,ASSOCIATE]',
+            'errors' => [
+                'required' => 'Please select a role.',
+                'in_list' => 'Please select a valid role.'
+            ]
+        ]
+    ];
+
 }
