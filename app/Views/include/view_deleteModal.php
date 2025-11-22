eb2073f80b8630eefe07cf72656d2b6be67ad798
a-manalo <agmnlo.by@gmail.com>
2025-11-23 03:52:58 +0800

    added delete modal

---
+++ app/Views/include/view_deleteModal.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content custom-modal">
+      <div class="modal-body text-center">
+        <div class="icon-circle mb-3">!</div>
+        <h5 class="fw-semibold mb-2">Are you sure?</h5>
+        <p class="text-muted mb-4">This action can’t be undone. Please confirm if you want to proceed.</p>
+        <div class="d-flex justify-content-center gap-3">
+          <button type="button" class="btn cancel-btn" data-bs-dismiss="modal">Cancel</button>
+          <a href="#" id="confirmDeleteBtn" class="btn confirm-btn">Confirm</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
