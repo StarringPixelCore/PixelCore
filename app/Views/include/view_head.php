e7fc1ccdf6a3a12cf4b0c99b53bd8dc66c6157d9
a-manalo <agmnlo.by@gmail.com>
2025-11-25 02:49:58 +0800

    feat(view_head.php): added Bootstrap JS and delete modal CSS

--- app/Views/include/view_head.php
+++ app/Views/include/view_head.php
@@ -6,9 +6,13 @@
     <!-- BOOTSTRAP CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <!-- BOOTSTRAP ICONS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <!-- CUSTOM CSS -->
     <link rel="stylesheet" href="<?= base_url('public/css/style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('public/css/deleteModal.css') ?>">
 </head>
