85b22537ec8b7d84c228d6049e77522c94927c5f
Your Name <your.email@example.com>
2025-11-29 20:08:09 +0800

    feat(view_users.php): Corrected css path

--- app/Views/users/view_users.php
+++ app/Views/users/view_users.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?= view('include/view_head', ['title' => $title ?? 'Users List']) ?>
-<link rel="stylesheet" href="<?= base_url('public/css/users.css') ?>">
+<link rel="stylesheet" href="<?= base_url('public/css/equipments.css') ?>">
 
 <body>
 <div class="wrapper">
